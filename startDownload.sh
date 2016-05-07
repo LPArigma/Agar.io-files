083cdcb2dc48240926bfcef4d6c18306d39e42f5
ItzLevvie <ItzLevvie@users.noreply.github.com>
2016-05-07 20:58:36 +0100

    Create startDownload.sh

---
+++ startDownload.sh
@@ -0,0 +1,93 @@
+mkdir css
+wget agar.io/css/master.css
+mv *.css css
+mkdir fonts
+wget agar.io/fonts/glyphicons-halflings-regular.eot
+wget agar.io/fonts/glyphicons-halflings-regular.svg
+wget agar.io/fonts/glyphicons-halflings-regular.ttf
+wget agar.io/fonts/glyphicons-halflings-regular.woff
+wget agar.io/fonts/glyphicons-social-regular.eot
+wget agar.io/fonts/glyphicons-social-regular.svg
+wget agar.io/fonts/glyphicons-social-regular.ttf
+wget agar.io/fonts/glyphicons-social-regular.woff
+wget agar.io/fonts/glyphicons-social-regular.woff2
+mv *.eot fonts
+mv *.svg fonts
+mv *.ttf fonts
+mv *.woff fonts
+mv *.woff2 fonts
+mkdir img
+wget agar.io/img/1200x630.png
+wget agar.io/img/appstore.png
+wget agar.io/img/background.png
+wget agar.io/img/controller.png
+wget agar.io/img/google_play.png
+wget agar.io/img/incentive_hoc.png
+wget agar.io/img/login-perks.png
+wget agar.io/img/promo_hoc.png
+wget agar.io/img/split.png
+wget agar.io/img/star.png
+mv *.png img
+mkdir js
+wget agar.io/js/a.js
+wget agar.io/js/master.js
+mv *.js js
+mkdir mc
+cd mc
+mkdir atlas
+wget agar.io/mc/atlas/main_shop.png
+wget agar.io/mc/atlas/main_shop.xml
+mv *.png atlas
+mv *.xml atlas
+mkdir core-assets
+wget agar.io/mc/core-assets/Close.png
+wget agar.io/mc/core-assets/Close_disabled.png
+wget agar.io/mc/core-assets/oaktree.png
+mv *.png core-assets
+mkdir img
+wget agar.io/mc/img/Close.png
+wget agar.io/mc/img/Close_disabled.png
+wget agar.io/mc/img/Close_disabled_old.png
+wget agar.io/mc/img/Close_old.png
+wget agar.io/mc/img/Exp_Star.png
+wget agar.io/mc/img/FB_Arrow.png
+wget agar.io/mc/img/Facebook_Share_BTN.png
+wget agar.io/mc/img/Facebook_Symbol.png
+wget agar.io/mc/img/Login_Play.png
+wget agar.io/mc/img/Twitter_Share_BTN.png
+wget agar.io/mc/img/arrow.png
+wget agar.io/mc/img/back_button.png
+wget agar.io/mc/img/currency_icon.png
+wget agar.io/mc/img/fast_play.png
+wget agar.io/mc/img/giant_coin.png
+wget agar.io/mc/img/hint.png
+wget agar.io/mc/img/incentive_android+iphone.png
+wget agar.io/mc/img/incentive_android.png
+wget agar.io/mc/img/incentive_android_button.png
+wget agar.io/mc/img/incentive_iphone_button.png
+wget agar.io/mc/img/massboost_2x_shop.png
+wget agar.io/mc/img/massboost_3x_shop.png
+wget agar.io/mc/img/multiplier_stamp.png
+wget agar.io/mc/img/not_for_guests.png
+wget agar.io/mc/img/play.png
+wget agar.io/mc/img/profilepic_guest.png
+wget agar.io/mc/img/sticker.png
+wget agar.io/mc/img/xpboost_2x_shop.png
+wget agar.io/mc/img/xpboost_3x_shop.png
+mv *.png img
+mkdir particle
+wget agar.io/mc/particle/LevelUpSlotBar-ipadhd.plist
+wget agar.io/mc/particle/LevelUpSlotBarParticle.png
+mv *.plist particle
+mv *.png particle
+wget agar.io/mc/agario.js
+cd ~
+wget agar.io/agario.core.js
+wget agar.io/blocker.png
+wget agar.io/changelog.txt
+wget agar.io/environment.js
+wget agar.io/favicon-32x32.png
+wget agar.io/i18n.js
+wget agar.io/main_out.js
+wget agar.io/privacy.txt
+wget agar.io/terms.txt
